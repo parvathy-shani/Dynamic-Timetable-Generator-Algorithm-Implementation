2702913237c9f67b0fd6bb77d1759d55fa5c0c77
parvathy-shani <76484421+parvathy-shani@users.noreply.github.com>
2024-04-18 20:08:13 +0530

    Add files via upload

---
+++ Step7_Format_events.py
@@ -0,0 +1,35 @@
+import json
+
+# Load data from data.json
+with open('combined_data.json', 'r') as json_file:
+    data = json.load(json_file)
+
+# Load teacher-course mapping from events.json
+with open('events.json', 'r') as event_file:
+    teacher_course_mapping = json.load(event_file)
+
+print(teacher_course_mapping)
+
+valid_events = []
+#invalid_events = []
+
+for mapping in teacher_course_mapping:
+    teacher_code, course_info = mapping[0], mapping[1]
+    print(teacher_code, course_info)
+    found = False
+    for course in data:
+         if course_info.startswith(course['Course code']):
+             sem_code = course_info.split('-')[1]
+             batch = course_info.split('-')[2]
+             if any(teacher['teacher_code'] == teacher_code for teacher in course['lecturers']):
+                 valid_event = f"{teacher_code} {course['Course code']}-{sem_code}-{batch}"
+                 valid_events.append(valid_event)
+                 found = True
+                 break
+
+
+#Save all combinations to a new file
+file_path = "valid_events.json"     # Output file to store the output
+with open(file_path, "w") as json_file:  # Open the file to write the output
+    json.dump(valid_events, json_file, indent=4)  # Storing the timeslots into json file
+
